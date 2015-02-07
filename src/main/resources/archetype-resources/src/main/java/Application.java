d1c216e2f70cb68f2453428a90339e6ecc9cb6f5
Deven Phillips <deven.phillips@gmail.com>
2015-02-07 12:06:21 -0500

    Several updates

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,27 +1,20 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ${package};
 
-import java.util.HashSet;
 import java.util.Set;
+import javax.ws.rs.Path;
+import org.reflections.Reflections;
 
-/**
- *
- * @author dphillips
- */
 public class Application extends javax.ws.rs.core.Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> rrcs = new HashSet<>();
-        rrcs.add(ExampleRest.class);
-        // Add JAX-RS Annotated classes to the HashSet
-        return rrcs;
+        // Use the reflections library to scan the current package tree for
+        // classes annotated with javax.ws.rs.Path and add them to the JAX-RS
+        // application
+        Reflections reflections = new Reflections(this.getClass().getPackage().getName());
+        return reflections.getTypesAnnotatedWith(Path.class);
     }
 }
--- src/main/resources/archetype-resources/src/test/java/ExampleRestTest.java
+++ src/main/resources/archetype-resources/src/test/java/ExampleRestTest.java
@@ -1,21 +1,12 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ${package};
 
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- *
- * @author dphillips
- */
 public class ExampleRestTest {
 
     ExampleRest api;
