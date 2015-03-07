909ec925f3499f0ea8c72b79b43c8bb2d397481e
Deven Phillips <deven.phillips@gmail.com>
2015-03-06 19:29:53 -0500

    Updated WELD version and Bootstrap CDI in test

--- src/main/resources/archetype-resources/src/test/java/ExampleRestTest.java
+++ src/main/resources/archetype-resources/src/test/java/ExampleRestTest.java
@@ -3,17 +3,36 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import junit.framework.Assert;
+import org.apache.deltaspike.cdise.api.CdiContainer;
+import org.apache.deltaspike.cdise.api.CdiContainerLoader;
+import org.apache.deltaspike.cdise.api.ContextControl;
+import org.apache.deltaspike.core.api.provider.BeanProvider;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.restlet.ext.jaxrs.InstantiateException;
+import org.restlet.ext.jaxrs.ObjectFactory;
 
 public class ExampleRestTest {
 
     ExampleRest api;
+    CdiContainer cdiContainer;
 
     @Before
-    public void beforeMethod() {
-        api = new ExampleRest();
+    public void before() throws InstantiateException {
+        cdiContainer = CdiContainerLoader.getCdiContainer();
+        cdiContainer.boot();
+
+        // Starting the all contexts
+        ContextControl contextControl = cdiContainer.getContextControl();
+        contextControl.startContexts();
+        api = (new CdiObjectFactory()).getInstance(ExampleRest.class);
+    }
+
+    @After
+    public void after() {
+        cdiContainer.shutdown();
     }
 
     @Test
