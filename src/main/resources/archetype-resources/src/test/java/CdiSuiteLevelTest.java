b36d87da2de49b748025d582e89153345e8b199f
Deven Phillips <deven.phillips@gmail.com>
2015-03-06 22:45:05 -0500

    Removed my name from the source files

--- src/main/resources/archetype-resources/src/test/java/CdiSuiteLevelTest.java
+++ src/main/resources/archetype-resources/src/test/java/CdiSuiteLevelTest.java
@@ -7,10 +7,6 @@ import org.apache.deltaspike.testcontrol.api.junit.CdiTestSuiteRunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- *
- * @author <a href="">Deven Phillips</a>
- */
 @RunWith(CdiTestSuiteRunner.class)
 @Suite.SuiteClasses({ExampleRestTest.class})
 public class CdiSuiteLevelTest {
